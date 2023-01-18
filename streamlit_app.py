8e312fdf1dded75a158ec048c5ec45b5a61a2207
Dave T <winkinglion@gmail.com>
2023-01-18 16:29:28 -0700

    Update streamlit_app.py
    testing different text syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,8 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
-st.text('Omega 3\nKale\nHard')
+st.text('Omega 3\n
+        Kale\n
+        Hard'
+)
 
