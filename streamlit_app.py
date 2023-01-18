bfec55280438cf4072bf832914bf8418a709b0f0
Dave T <winkinglion@gmail.com>
2023-01-18 16:30:19 -0700

    Update streamlit_app.py
    removing line break test that streamlit did NOT like

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,5 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
-st.text('Omega 3\n
-        Kale\n
-        Hard'
-)
+st.text('Omega 3 \nKale \nHard')
 
