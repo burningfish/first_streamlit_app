a347a1800a594648c054ea98d39be6d368e29cad
Dave T <winkinglion@gmail.com>
2023-01-19 15:11:51 -0700

    Update streamlit_app.py
    switching the fruit list index to be Fruit name giving using a more meaningful picklist

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pa
 my_fruit_list = pa.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit') 
 
 # Pick list so users can pick what fruits they want
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
