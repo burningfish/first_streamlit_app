20abc5626b295c13204c89a007ea66e529478d73
Dave T <winkinglion@gmail.com>
2023-01-20 12:53:51 -0700

    Update streamlit_app.py
    forgot that I need to use my streamlit alias
    added header for fruityvice section

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
+st.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+st.text(fruityvice_response)
 
 
