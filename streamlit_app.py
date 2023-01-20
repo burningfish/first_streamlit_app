bdec181c42506d381f8286a0386291cfb6e19984
Dave T <winkinglion@gmail.com>
2023-01-20 16:14:50 -0700

    Update streamlit_app.py
    separate data types to get correct results

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,8 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-st.text("The fruit load list contains: " + my_data_row)
+st.text("The fruit load list contains: ")
+st.text(my_data_row)
 
 
 
